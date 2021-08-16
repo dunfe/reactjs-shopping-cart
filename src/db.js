7dcd3da89b05152a1db986dcc56ce7307413e047
Dũn <qngnud@gmail.com>
2021-08-16 16:25:44 +0700

    Update db.js

--- src/db.js
+++ src/db.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import ReduxSagaFirebase from "redux-saga-firebase";
 
 const fb = firebase.initializeApp({
-  apiKey: "AIzaSyCbB1zOg39AjUXVMSFc5n37SrVDv5a9P1M",
-  authDomain: "react-shopping-cart-f09d2.firebaseapp.com",
-  databaseURL: "https://react-shopping-cart-f09d2.firebaseio.com",
-  projectId: "react-shopping-cart-f09d2",
-  storageBucket: "react-shopping-cart-f09d2.appspot.com",
-  messagingSenderId: "237760961127",
-  appId: "1:237760961127:web:de5211552408d772e212c2",
-  measurementId: "G-NCL2E36HTQ"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 });
 export const reduxSagaFirebase = new ReduxSagaFirebase(fb);
